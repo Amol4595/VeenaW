ea4f0ece96f569fdb3efadbacaed2025ec0a08da
nidhi jeena <52296061+nidhijeena@users.noreply.github.com>
2021-06-16 11:22:01 +0530

    update

--- Internship/frontend/src/app/app.module.ts
+++ Internship/frontend/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
+
 import { MapsComponent } from './maps/maps.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
+ 
     MapsComponent,
   
   ],
